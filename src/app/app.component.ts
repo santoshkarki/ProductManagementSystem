2b2ea857654f7f7537bc286bf60b721b4c2eccaa
santosh <santosh.kaarki@gmail.com>
2017-12-03 16:02:16 -0600

    data binding added: Interpolation+property binding +event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pageTitle : String = 'Acme Product Management';
+  pageTitle : String = 'Our Product Management';
 }
