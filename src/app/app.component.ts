238740e6145e2215a3d26130c3d591041edb7b3e
santosh <santosh.kaarki@gmail.com>
2017-12-04 17:34:21 -0600

    adding service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductService } from './product-list/product.service';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[ProductService]
 })
 export class AppComponent {
   pageTitle : String = 'Our Product Management';
